e7b2a7508ad7b4559639c11d90119992312d8792
William Hammond <william.hammond@okstate.edu>
2020-05-16 02:51:01 -0500

    image size=100%

--- XFT/app.R
+++ XFT/app.R
@@ -79,7 +79,7 @@ shinyApp(
         tabsetPanel(
           tabPanel("Home Page",
                    mainPanel(tags$h4("Welcome to the Xylem Functional Traits Database portal")),
-                     img(src='Picture1.png',height=728,width=829),
+                     img(src='Picture1.png',height="100%",width="100%"),
                              includeHTML("welcome.html")),
           tabPanel("Trait Definitions",
                    mainPanel(tags$h4("Trait definitions"),
